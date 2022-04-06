20c9266524b02c32f6165a9f6e790f85233876af
Lavinia Fonte <lavifonte@hotmail.com>
2022-04-06 15:23:24 -0300

    fix connection string

--- TechnicalShare/TechnicalShare/Data/SeedingService.cs
+++ TechnicalShare/TechnicalShare/Data/SeedingService.cs
@@ -22,8 +22,8 @@ namespace TechnicalShare.Data
             {
                 Expertise expertise = new Expertise("front-end");
                 Expertise expertise2 = new Expertise("back-end");
-                Mentor mentor1 = new Mentor(3, "Billy", "billy@hotmail.com", "1234567", "desenvolvedor front-end", Models.Enums.Level.MidLevel, expertise);
-                Mentor mentor2 = new Mentor(4, "Anna", "anna@hotmail.com", "1234567", "desenvolvedora back-end", Models.Enums.Level.Junior, expertise2);
+                Mentor mentor1 = new Mentor(1, "Billy", "billy@hotmail.com", "1234567", "desenvolvedor front-end", Models.Enums.Level.MidLevel, expertise);
+                Mentor mentor2 = new Mentor(2, "Anna", "anna@hotmail.com", "1234567", "desenvolvedora back-end", Models.Enums.Level.Junior, expertise2);
 
                 _context.Expertise.AddRange(expertise, expertise2);
                 _context.Mentor.AddRange(mentor1, mentor2);
