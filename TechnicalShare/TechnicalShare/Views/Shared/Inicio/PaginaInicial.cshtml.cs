b97dddf871774543b1e5de181773cdd200a9e985
Henrique Galieta <100798441+HGalieta@users.noreply.github.com>
2022-04-06 15:54:15 -0300

    Commiting correct file.
    Sorry...

---
+++ TechnicalShare/TechnicalShare/Views/Shared/Inicio/PaginaInicial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TechnicalShare.Views.Home
+{
+    public class PaginaInicialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
