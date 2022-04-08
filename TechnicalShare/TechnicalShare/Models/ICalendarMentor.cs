74bfce58689780b3d0003bff965879e7897bcf19
Henrique Galieta <100798441+HGalieta@users.noreply.github.com>
2022-04-08 20:02:09 -0300

    New Interface
    For application of dependency injection.

---
+++ TechnicalShare/TechnicalShare/Models/ICalendarMentor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TechnicalShare.Models
+{
+    public interface ICalendarMentor
+    {
+        DateTime dateTimeMentor { get; set; }
+        Mentor Mentor { get; set; }
+        int MentorId { get; set; }
+    }
+}
