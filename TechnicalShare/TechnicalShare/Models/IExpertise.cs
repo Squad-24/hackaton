611b5f4c2183900ed09c7daa5571403bf2dd5390
Henrique Galieta <100798441+HGalieta@users.noreply.github.com>
2022-04-08 20:04:55 -0300

    New Interface
    For application of dependency injection

---
+++ TechnicalShare/TechnicalShare/Models/IExpertise.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace TechnicalShare.Models
+{
+    public interface IExpertise
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        public ICollecion<Mentor> Mentors { get; set; }
+        void AddMentor(Mentor mentor);
+    }
+}
