07bde34e8ae7682544b551f93724867e6bca0edd
Henrique Galieta <100798441+HGalieta@users.noreply.github.com>
2022-04-08 19:59:37 -0300

    Dependency Injection

--- TechnicalShare/TechnicalShare/Models/Expertise.cs
+++ TechnicalShare/TechnicalShare/Models/Expertise.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace TechnicalShare.Models
 {
-    public class Expertise
+    public class Expertise : IExpertise
     {
         public int Id { get; set; }
         public string Name { get; set; }
