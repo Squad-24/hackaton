45889cfe7134a365bef7f63e11e87ae24ed7fcf9
Henrique Galieta <100798441+HGalieta@users.noreply.github.com>
2022-04-08 20:25:56 -0300

    New Interface
    For dependency injection

---
+++ TechnicalShare/TechnicalShare/Services/IMentorService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using TechnicalShare.Models;
+
+namespace TechnicalShare.Services
+{
+    public interface IMentorService
+    {
+        Mentor AddMentor(Mentor mentor);
+        List<Mentor> FindAll();
+        List<Mentor> FilterById(int Id);
+        Mentor FindbyId(int id);
+    }
+}
