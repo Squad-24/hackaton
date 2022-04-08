bbefd0b0c903dbcc172b2d6a06eff3727b75df0f
Henrique Galieta <100798441+HGalieta@users.noreply.github.com>
2022-04-08 20:17:19 -0300

    New Interface
    For Dependency Injection

---
+++ TechnicalShare/TechnicalShare/Models/IMentorMentee.cs
@@ -0,0 +1,11 @@
+﻿namespace TechnicalShare.Models
+{
+    public interface IMentorMentee
+    {
+        Expertise Expertise { get; set; }
+        Mentee Mentee { get; set; }
+        int MenteeId { get; set; }
+        Mentor Mentor { get; set; }
+        int MentorId { get; set; }
+    }
+}
