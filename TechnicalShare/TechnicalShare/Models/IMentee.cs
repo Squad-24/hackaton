9f1d952a25bc0c69cc41cdabfcd8819f28112779
Henrique Galieta <100798441+HGalieta@users.noreply.github.com>
2022-04-08 20:12:26 -0300

    New Interface
    For dependency injection

---
+++ TechnicalShare/TechnicalShare/Models/IMentee.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace TechnicalShare.Models
+{
+    public interface IMentee
+    {
+        string Email { get; set; }
+        int Id { get; set; }
+        ICollection<MentorMentee> Mentors { get; set; }
+        string Name { get; set; }
+
+        void AddMentor(MentorMentee mentor);
+        void RemoveMentor(MentorMentee mentor);
+    }
+}
