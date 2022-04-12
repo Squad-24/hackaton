722ac422ff1bdefcc162a3dfa26ea62e6101a543
Lavinia Fonte <lavifonte@hotmail.com>
2022-04-12 02:41:29 -0300

    more expertise tags

--- TechnicalShare/TechnicalShare/Controllers/ExpertisesController.cs
+++ TechnicalShare/TechnicalShare/Controllers/ExpertisesController.cs
@@ -11,13 +11,13 @@ using TechnicalShare.Services;
 namespace TechnicalShare.Controllers
 {
     public class ExpertisesController : Controller
-    {        
-        private readonly IExpertiseService _expertiseService;       
+    {
+        private readonly IExpertiseService _expertiseService;
         public ExpertisesController(IExpertiseService expertiseService)
         {
-            
+
             _expertiseService = expertiseService;
-           
+
         }
         public IActionResult Index()
         {
@@ -25,47 +25,126 @@ namespace TechnicalShare.Controllers
             return View(list);
         }
 
-        public IActionResult BackEnd()
+        public IActionResult Adobe()
         {
-            var list = _expertiseService.FilterById(1);                       
+            var list = _expertiseService.FilterById(1);
             return View(list);
-        }              
-
-        public IActionResult DevOps()
+        }
+        public IActionResult BackEnd()
         {
             var list = _expertiseService.FilterById(2);
             return View(list);
         }
 
-        public IActionResult FrontEnd()
+        public IActionResult CloudComputing()
         {
             var list = _expertiseService.FilterById(3);
             return View(list);
         }
 
-        public IActionResult Mobile()
+        public IActionResult Csharp()
         {
             var list = _expertiseService.FilterById(4);
             return View(list);
         }
 
-        public IActionResult ProductManagement()
+        public IActionResult DataScience()
         {
             var list = _expertiseService.FilterById(5);
             return View(list);
         }
-
-        public IActionResult UIDesign()
+        public IActionResult DevOps()
         {
             var list = _expertiseService.FilterById(6);
             return View(list);
         }
 
-        public IActionResult UXDesign()
+        public IActionResult FrontEnd()
         {
             var list = _expertiseService.FilterById(7);
             return View(list);
         }
+
+        public IActionResult HtmlCss()
+        {
+            var list = _expertiseService.FilterById(8);
+            return View(list);
+        }
+        public IActionResult Java()
+        {
+            var list = _expertiseService.FilterById(9);
+            return View(list);
+        }
+
+        public IActionResult Javascript()
+        {
+            var list = _expertiseService.FilterById(10);
+            return View(list);
+        }
+
+        public IActionResult Mobile()
+        {
+            var list = _expertiseService.FilterById(11);
+            return View(list);
+        }
+
+        public IActionResult NodeJs()
+        {
+            var list = _expertiseService.FilterById(12);
+            return View(list);
+        }
+
+        public IActionResult Php()
+        {
+            var list = _expertiseService.FilterById(13);
+            return View(list);
+        }
+        public IActionResult ProductDesign()
+        {
+            var list = _expertiseService.FilterById(14);
+            return View(list);
+        }
+
+
+        public IActionResult Python()
+        {
+            var list = _expertiseService.FilterById(15);
+            return View(list);
+        }
+        public IActionResult React()
+        {
+            var list = _expertiseService.FilterById(16);
+            return View(list);
+        }
+
+
+        public IActionResult TypeScript()
+        {
+            var list = _expertiseService.FilterById(17);
+            return View(list);
+        }
+
+
+        public IActionResult UIUX()
+        {
+            var list = _expertiseService.FilterById(18);
+            return View(list);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
--- TechnicalShare/TechnicalShare/Data/SeedingService.cs
+++ TechnicalShare/TechnicalShare/Data/SeedingService.cs
@@ -23,24 +23,38 @@ namespace TechnicalShare.Data
                 return;
             }
 
-            Expertise expertise = new Expertise(1, "back-end");
-            Expertise expertise2 = new Expertise(2, "devops");
-            Expertise expertise3 = new Expertise(3, "front-end");
-            Expertise expertise4 = new Expertise(4, "mobile");
-            Expertise expertise5 = new Expertise(5, "product management");
-            Expertise expertise6 = new Expertise(6, "ui design");
-            Expertise expertise7 = new Expertise(7, "ux design");            
+            Expertise expertise = new Expertise(1, "adobe");
+            Expertise expertise2 = new Expertise(2, "back-end");
+            Expertise expertise3 = new Expertise(3, "cloud computing");
+            Expertise expertise4 = new Expertise(4, "c#");
+            Expertise expertise5 = new Expertise(5, "data science");
+            Expertise expertise6 = new Expertise(6, "devops");
+            Expertise expertise7 = new Expertise(7, "front-end");
+            Expertise expertise8 = new Expertise(8, "html e css");
+            Expertise expertise9 = new Expertise(9, "java");
+            Expertise expertise10 = new Expertise(10, "javascript");
+            Expertise expertise11 = new Expertise(11, "mobile");
+            Expertise expertise12 = new Expertise(12, "node.js");
+            Expertise expertise13 = new Expertise(13, "php");
+            Expertise expertise14 = new Expertise(14, "product design");
+            Expertise expertise15 = new Expertise(15, "python");
+            Expertise expertise16 = new Expertise(16, "react");
+            Expertise expertise17 = new Expertise(17, "ui/ux design");
+
+
+
             Mentor mentor2 = new Mentor("maria", "maria@hotmail.com", "1234567", "desenvolvedora full stack", Models.Enums.Level.senior, expertise);
             Mentor mentor6 = new Mentor("mariana", "mariana@hotmail.com", "1234567", "devops", Models.Enums.Level.senior, expertise2);
             Mentor mentor1 = new Mentor("joão", "joão@hotmail.com", "1234567", "desenvolvedor front-end", Models.Enums.Level.pleno, expertise3);
             Mentor mentor3 = new Mentor("paula", "paula@hotmail.com", "1234567", "desenvolvedora mobile", Models.Enums.Level.junior, expertise4);
             Mentor mentor4 = new Mentor("jorge", "jorge@hotmail.com", "1234567", "ui designer", Models.Enums.Level.pleno, expertise6);
-            Mentor mentor5 = new Mentor("gustavo", "gustavo@hotmail.com", "1234567", "ux designer", Models.Enums.Level.senior, expertise7);            
+            Mentor mentor5 = new Mentor("gustavo", "gustavo@hotmail.com", "1234567", "ux designer", Models.Enums.Level.senior, expertise7);
             Mentor mentor7 = new Mentor("victor", "victor@hotmail.com", "1234567", "product manager", Models.Enums.Level.pleno, expertise5);
 
 
 
-            _context.Expertise.AddRange(expertise, expertise2, expertise3, expertise4, expertise5, expertise6, expertise7);
+            _context.Expertise.AddRange(expertise, expertise2, expertise3, expertise4, expertise5, expertise6, expertise7, expertise8,
+                expertise9, expertise10, expertise11, expertise12, expertise13, expertise14, expertise15, expertise16, expertise17);
             _context.Mentor.AddRange(mentor1, mentor2, mentor3, mentor4, mentor5, mentor6, mentor7);
             _context.SaveChanges();
         }
